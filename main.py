6c7940287636cb61dca79e3aff6522c7f1deda44
PaulKMueller <paul.mueller@quantco.com>
2024-10-27 13:08:59 +0100

    Tab config

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import streamlit as st
 import segno
 
+# Set tab title
+
+st.set_page_config(page_title='Open QR', page_icon='openqr_tab_icon.png', layout='centered', initial_sidebar_state='auto')
+
 st.title('Open QR')
 
 url_input = st.text_input('Enter the URL of the QR code image:')
